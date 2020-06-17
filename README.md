79ed65c205cfff9a024bac81241a368fbef37bd2
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-06-18 00:47:52 +0900

    routine

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# concept
 Please read my article in Qiita!
+
+
--- code.js
+++ code.js
@@ -1,1 +1,10 @@
-function myFunction() {Logger.log("Hello World");}
+function myFunction() {
+    const token = PropertiesService.getScriptProperties().getProperty("token");
+    const account = "aichan_nel"
+    const count = 2;
+    const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${account}&count=${count}`;
+    const headers = {"Authorization": `Bearer ${token}`}
+    response = UrlFetchApp.fetch(url, {"headers": headers});
+    console.log(response.getContentText());
+    // GmailApp.sendEmail(mail, "morning_ai_bot", url)
+}
