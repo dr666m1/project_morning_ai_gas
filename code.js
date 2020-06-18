4a2df68b133686a533ac679c76286b49d6378a71
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-06-18 22:54:53 +0900

    routine

--- code.js
+++ code.js
@@ -1,10 +1,21 @@
-function myFunction() {
-    const token = PropertiesService.getScriptProperties().getProperty("token");
-    const account = "aichan_nel"
-    const count = 2;
-    const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${account}&count=${count}`;
-    const headers = {"Authorization": `Bearer ${token}`}
-    response = UrlFetchApp.fetch(url, {"headers": headers});
-    console.log(response.getContentText());
-    // GmailApp.sendEmail(mail, "morning_ai_bot", url)
+function morning_ai_bot() {
+  const token = PropertiesService.getScriptProperties().getProperty("token");
+  const mail = PropertiesService.getScriptProperties().getProperty("mail");
+  const account = "aichan_nel";
+  const count = 3;
+  const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${account}&count=${count}`;
+  const headers = {"Authorization": `Bearer ${token}`};
+  const response = JSON.parse(UrlFetchApp.fetch(url, {"headers": headers}).getContentText());
+  let flg = 0;
+  for (let i of response) {
+    for (let j of i.entities.hashtags) {
+      if (j.text === "モーニングアイちゃん") {
+        const tweet_id = i.id_str;
+        GmailApp.sendEmail(mail, "morning_ai_bot", `https://twitter.com/${account}/status/${tweet_id}`)
+        flg = 1;
+        break;
+      }
+    }
+    if (flg === 1) break;
+  }
 }
