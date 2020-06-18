67b79eb098a973079c225b4d9415bbe1c4ce9cb8
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-06-18 22:56:14 +0900

    routine

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function morning_ai_bot() {
   const token = PropertiesService.getScriptProperties().getProperty("token");
   const mail = PropertiesService.getScriptProperties().getProperty("mail");
   const account = "aichan_nel";
-  const count = 3;
+  const count = 5;
   const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${account}&count=${count}`;
   const headers = {"Authorization": `Bearer ${token}`};
   const response = JSON.parse(UrlFetchApp.fetch(url, {"headers": headers}).getContentText());
